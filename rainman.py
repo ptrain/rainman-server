e33e82773bee9ef3519262a3f778cd0a940d60bd
Adam Moore <AMoore913@gmail.com>
2014-11-02 22:37:19 -0500

    Make API call with brooklyn long/lat values, return json

--- rainman.py
+++ rainman.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 import forecastio
 from flask import Flask 
@@ -7,11 +8,18 @@ API_KEY = os.environ.get('DARKSKY_API_KEY')
 if not API_KEY:
     raise ApiKeyNotFoundError('Please ensure the DARKSKY_API_KEY environment variable is set.')
 
+# TODO: In future, we will of course have these as route parameters
+BROOKLYN_LAT = 40.69
+BROOKLYN_LNG = -73.93
+
 app = Flask(__name__)
 
 @app.route('/')
 def get_rain_data():
-    return "It's GONE RAIN"
+    forecast = forecastio.load_forecast(API_KEY, BROOKLYN_LAT, BROOKLYN_LNG)
+    byHour = forecast.hourly()
+    data = [{'cloudCover': hourData.cloudCover, 'precipProbability': hourData.precipProbability} for hourData in byHour.data[0:24]]
+    return json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
 
 if __name__ == '__main__':
     app.run(debug=True)
